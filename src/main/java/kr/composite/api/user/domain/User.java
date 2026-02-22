4fc90ae8e4deb3ceda6c4ff1526dea65f2caad13
KimDaehyeon <daehyeon3351@gmail.com>
2026-02-23 01:52:18 +0900

    feat: User Entity 구현
    Co-authored-by: supernovamk <minkikim2002@gmail.com>

---
+++ src/main/java/kr/composite/api/user/domain/User.java
@@ -0,0 +1,19 @@
+package kr.composite.api.user.domain;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import kr.composite.api.global.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Embedded
+    private UserName name;
+}
