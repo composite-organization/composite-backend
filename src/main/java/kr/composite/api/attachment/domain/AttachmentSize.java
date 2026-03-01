90b71d05bdce3cfcf3357c20dcfcab4b07887379
KimDaehyeon <daehyeon3351@gmail.com>
2026-03-01 17:26:05 +0900

    feat: 누락된 Embeddable 어노테이션 추가

--- src/main/java/kr/composite/api/attachment/domain/AttachmentSize.java
+++ src/main/java/kr/composite/api/attachment/domain/AttachmentSize.java
@@ -1,11 +1,16 @@
 package kr.composite.api.attachment.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AttachmentSize {
 
     @Column(name = "size")
-    private final String value;
+    private String value;
 
     public AttachmentSize(String value) {
         this.value = value;
