fa95e7dc22b9cefb91333f6901864d2119f3ceae
KimDaehyeon <daehyeon3351@gmail.com>
2026-03-01 17:32:55 +0900

    feat: QuizOption의 누락된 컬럼 어노테이션 추가

--- src/main/java/kr/composite/api/quiz/domain/QuizOption.java
+++ src/main/java/kr/composite/api/quiz/domain/QuizOption.java
@@ -17,10 +17,12 @@ public class QuizOption extends BaseEntity {
     @Column(name = "quiz_widget_id")
     private Long quizWidgetId;
 
+    @Column(name = "sequence")
     private Long sequence;
 
+    @Column(name = "content")
     private String content;
 
+    @Column(name = "is_correct")
     private boolean isCorrect;
-
 }
