69befdf688ca0f627f5b2937da9abbe4985a03b4
KimDaehyeon <daehyeon3351@gmail.com>
2026-03-02 16:48:51 +0900

    feat: 수업 자료 파일 크기 단위 Enum 구현

---
+++ src/main/java/kr/composite/api/attachment/domain/AttachmentUnit.java
@@ -0,0 +1,40 @@
+package kr.composite.api.attachment.domain;
+
+import jakarta.persistence.AttributeConverter;
+
+import java.util.Arrays;
+
+public enum AttachmentUnit {
+    KB("KB"),
+    MB("MB");
+
+    private final String description;
+
+    AttachmentUnit(String description) {
+        this.description = description;
+    }
+
+    static class Converter implements AttributeConverter<AttachmentUnit, String> {
+
+        @Override
+        public String convertToDatabaseColumn(AttachmentUnit attachmentUnit) {
+            if (attachmentUnit == null) {
+                return null;
+            }
+
+            return attachmentUnit.description;
+        }
+
+        @Override
+        public AttachmentUnit convertToEntityAttribute(String dbData) {
+            if (dbData == null) {
+                return null;
+            }
+
+            return Arrays.stream(values())
+                    .filter(value -> value.description.equals(dbData))
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+    }
+}
