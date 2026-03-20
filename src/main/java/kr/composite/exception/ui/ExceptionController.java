7c2eca00a718f14a68b990ea1310d1fa4ba52e95
KimDaehyeon <daehyeon3351@gmail.com>
2026-03-20 19:45:43 +0900

    feat: 예외 처리 Advice 구현

---
+++ src/main/java/kr/composite/exception/ui/ExceptionController.java
@@ -0,0 +1,27 @@
+package kr.composite.exception.ui;
+
+import kr.composite.exception.domain.BusinessException;
+import kr.composite.exception.ui.dto.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handle(final BusinessException exception) {
+        ErrorResponse response = ErrorResponse.from(exception);
+        log.error("비즈니스 예외 {} 발생", response.code(), exception);
+        return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handle(final Exception exception) {
+        ErrorResponse response = ErrorResponse.from(exception);
+        log.error("예외 {} 발생", response.code(), exception);
+        return ResponseEntity.status(response.httpStatus()).body(response);
+    }
+}
