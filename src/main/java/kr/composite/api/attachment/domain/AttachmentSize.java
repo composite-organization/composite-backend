4a034c37db851400e43d8955b3140dc01058dbaa
supernovaMK <minkikim2002@gmail.com>
2026-03-23 15:59:49 +0900

    feat: AttachmentSize 파일 크기를 반환하는 메서드 구현

--- src/main/java/kr/composite/api/attachment/domain/AttachmentSize.java
+++ src/main/java/kr/composite/api/attachment/domain/AttachmentSize.java
@@ -9,10 +9,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AttachmentSize {
 
-    @Column(name = "size")
-    private int value;
+    private static final long KB_THRESHOLD = 1024L;
+    private static final long MB_THRESHOLD = 1024L * 1024L;
 
-    public AttachmentSize(int value) {
+    @Column(name = "size")
+    private Long value;
+
+    public AttachmentSize(Long value) {
         this.value = value;
+    }
+
+    // 1MB 이상이면 MB, 미만이면 KB
+    public AttachmentUnit getAppropriateUnit() {
+        if (this.value >= MB_THRESHOLD) {
+            return AttachmentUnit.MB;
+        }
+        return AttachmentUnit.KB;
+    }
+
+    public double getFormattedSize() {
+        AttachmentUnit unit = getAppropriateUnit();
+        if (unit == AttachmentUnit.MB) {
+            return (double) value / MB_THRESHOLD;
+        }
+        return (double) value / KB_THRESHOLD;
     }
 }
