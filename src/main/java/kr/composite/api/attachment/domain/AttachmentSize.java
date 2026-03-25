58abe3223031caec9b3e3b968cd3cc15565be4a1
supernovaMK <minkikim2002@gmail.com>
2026-03-26 00:31:08 +0900

    feat: 파일 단위를 구하는 로직 위치 변경

--- src/main/java/kr/composite/api/attachment/domain/AttachmentSize.java
+++ src/main/java/kr/composite/api/attachment/domain/AttachmentSize.java
@@ -3,14 +3,13 @@ package kr.composite.api.attachment.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AttachmentSize {
-
-    private static final long KB_THRESHOLD = 1024L;
-    private static final long MB_THRESHOLD = 1024L * 1024L;
 
     @Column(name = "size")
     private Long value;
@@ -19,19 +18,7 @@ public class AttachmentSize {
         this.value = value;
     }
 
-    // 1MB 이상이면 MB, 미만이면 KB
-    public AttachmentUnit getAppropriateUnit() {
-        if (this.value >= MB_THRESHOLD) {
-            return AttachmentUnit.MB;
-        }
-        return AttachmentUnit.KB;
-    }
-
-    public double getFormattedSize() {
-        AttachmentUnit unit = getAppropriateUnit();
-        if (unit == AttachmentUnit.MB) {
-            return (double) value / MB_THRESHOLD;
-        }
-        return (double) value / KB_THRESHOLD;
+    public double getFormattedSize(Long threshold) {
+        return (double) value / threshold;
     }
 }
