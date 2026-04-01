bd98b6e465a3561fa7cded6366c32ef2c1b11b24
supernovaMK <minkikim2002@gmail.com>
2026-04-01 22:38:32 +0900

    chore: 코드 스타일 정리

--- src/main/java/kr/composite/api/user/application/UserService.java
+++ src/main/java/kr/composite/api/user/application/UserService.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
     private final MemberRepository memberRepository;
     private final GuestRepository guestRepository;
