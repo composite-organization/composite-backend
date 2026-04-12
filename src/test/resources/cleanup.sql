be835b33570a0b268b4592516128869bd75d6d66
KimDaehyeon <daehyeon3351@gmail.com>
2026-04-13 06:04:56 +0900

    test: 투표 위젯 컨트롤러 테스트 제거

--- src/test/resources/cleanup.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM vote_submission;
-DELETE FROM vote_option;
-DELETE FROM vote_widget;
-DELETE FROM student;
-DELETE FROM widget;
--- src/test/resources/vote-test-student.sql
+++
@@ -1,3 +0,0 @@
-MERGE INTO student (id, user_id, lesson_id, name, created_at, updated_at, deleted)
-KEY (id)
-VALUES (1, 1, 1, '테스트학생', NOW(), NOW(), 0);
