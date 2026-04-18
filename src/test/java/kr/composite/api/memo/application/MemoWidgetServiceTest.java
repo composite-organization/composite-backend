5817b144898e3256644cf0cb71b1bbfe7755245f
supernovaMK <minkikim2002@gmail.com>
2026-04-18 14:34:45 +0900

    refactor: MemoWidget 관련 인가 과정 추가

--- src/test/java/kr/composite/api/memo/application/MemoWidgetServiceTest.java
+++ src/test/java/kr/composite/api/memo/application/MemoWidgetServiceTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import jakarta.persistence.EntityManager;
+import kr.composite.api.lesson.domain.Lesson;
+import kr.composite.api.lesson.domain.LessonCode;
+import kr.composite.api.lesson.domain.LessonName;
+import kr.composite.api.lesson.domain.LessonPassword;
 import kr.composite.api.memo.application.dto.request.MemoWidgetAddRequest;
 import kr.composite.api.memo.application.dto.request.MemoWidgetFindRequest;
 import kr.composite.api.memo.application.dto.request.MemoWidgetUpdateRequest;
@@ -12,8 +16,15 @@ import kr.composite.api.memo.domain.MemoContent;
 import kr.composite.api.memo.domain.MemoTitle;
 import kr.composite.api.memo.domain.MemoWidget;
 import kr.composite.api.memo.infrastructure.SpringDataJpaMemoWidgetRepository;
+import kr.composite.api.student.domain.Student;
+import kr.composite.api.student.domain.StudentName;
+import kr.composite.api.teacher.domain.Teacher;
+import kr.composite.api.teacher.domain.TeacherName;
+import kr.composite.api.user.domain.User;
+import kr.composite.api.user.domain.UserName;
 import kr.composite.api.widget.domain.Widget;
 import kr.composite.api.widget.domain.WidgetType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,10 +43,33 @@ class MemoWidgetServiceTest {
     @Autowired
     private MemoWidgetService memoWidgetService;
 
+    private User user;
+    private Lesson lesson;
+
+    @BeforeEach
+    void setUp() {
+        user = new User(new UserName("testUser"));
+        entityManager.persist(user);
+
+        lesson = new Lesson(new LessonName("lesson"), new LessonCode("code"), new LessonPassword("pass123"));
+        entityManager.persist(lesson);
+    }
+
+    private void makeUserTeacherOfLesson() {
+        Teacher teacher = new Teacher(user.getId(), lesson.getId(), new TeacherName("teacher"));
+        entityManager.persist(teacher);
+    }
+
+    private void makeUserStudentOfLesson() {
+        Student student = new Student(user.getId(), lesson.getId(), new StudentName("student"));
+        entityManager.persist(student);
+    }
+
     @Test
-    void 메모를_조회할_수_있다() {
+    void 선생님인_경우_메모를_조회할_수_있다() {
         // given
-        Widget widget = new Widget(null, WidgetType.MEMO);
+        makeUserTeacherOfLesson();
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
         entityManager.persist(widget);
         MemoTitle memoTitle = new MemoTitle("title");
         MemoContent memoContent = new MemoContent("content");
@@ -47,7 +81,7 @@ class MemoWidgetServiceTest {
 
         // when
         MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(memoWidget.getId());
-        MemoWidgetResponse memoWidgetResponse = memoWidgetService.getMemoWidget(memoWidgetFindRequest);
+        MemoWidgetResponse memoWidgetResponse = memoWidgetService.getMemoWidget(user, memoWidgetFindRequest);
 
         // then
         assertThat(memoWidgetResponse.id()).isEqualTo(memoWidget.getId());
@@ -57,25 +91,68 @@ class MemoWidgetServiceTest {
     }
 
     @Test
-    void 존재하지_않는_메모를_조회시_예외가_발생한다() {
+    void 학생인_경우_메모를_조회할_수_있다() {
         // given
-        MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(9999L);
+        makeUserStudentOfLesson();
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
+        entityManager.persist(widget);
+        MemoTitle memoTitle = new MemoTitle("title");
+        MemoContent memoContent = new MemoContent("content");
+        MemoWidget memoWidget = new MemoWidget(widget.getId(), memoTitle, memoContent);
+        entityManager.persist(memoWidget);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(memoWidget.getId());
+        MemoWidgetResponse memoWidgetResponse = memoWidgetService.getMemoWidget(user, memoWidgetFindRequest);
+
+        // then
+        assertThat(memoWidgetResponse.id()).isEqualTo(memoWidget.getId());
+        assertThat(memoWidgetResponse.title()).isEqualTo("title");
+    }
+
+    @Test
+    void 참여자가_아닌_경우_메모_조회시_예외가_발생한다() {
+        // given
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
+        entityManager.persist(widget);
+        MemoTitle memoTitle = new MemoTitle("title");
+        MemoContent memoContent = new MemoContent("content");
+        MemoWidget memoWidget = new MemoWidget(widget.getId(), memoTitle, memoContent);
+        entityManager.persist(memoWidget);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(memoWidget.getId());
+
         // when & then
-        assertThatThrownBy(() -> memoWidgetService.getMemoWidget(memoWidgetFindRequest)).isInstanceOf(
+        assertThatThrownBy(() -> memoWidgetService.getMemoWidget(user, memoWidgetFindRequest)).isInstanceOf(
                 MemoApplicationException.class);
     }
 
     @Test
-    void 메모를_생성할_수_있다() {
+    void 존재하지_않는_메모를_조회시_예외가_발생한다() {
         // given
+        MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(9999L);
+        // when & then
+        assertThatThrownBy(() -> memoWidgetService.getMemoWidget(user, memoWidgetFindRequest)).isInstanceOf(
+                MemoApplicationException.class);
+    }
 
-        Long lessonId = 1L;
+    @Test
+    void 선생님인_경우_메모를_생성할_수_있다() {
+        // given
+        makeUserTeacherOfLesson();
+
         String title = "메모 제목";
         String content = "메모 내용";
-        MemoWidgetAddRequest memoWidgetAddRequest = new MemoWidgetAddRequest(lessonId, title, content);
+        MemoWidgetAddRequest memoWidgetAddRequest = new MemoWidgetAddRequest(lesson.getId(), title, content);
 
         // when
-        MemoWidgetResponse memoWidgetResponse = memoWidgetService.addMemoWidget(memoWidgetAddRequest);
+        MemoWidgetResponse memoWidgetResponse = memoWidgetService.addMemoWidget(user, memoWidgetAddRequest);
 
         // then
         MemoWidget memoWidget = springDataJpaMemoWidgetRepository.findById(memoWidgetResponse.id()).orElse(null);
@@ -86,11 +163,25 @@ class MemoWidgetServiceTest {
     }
 
     @Test
-    void 메모를_수정할_수_있다() {
+    void 선생님이_아닌_경우_메모_생성시_예외가_발생한다() {
         // given
         String title = "메모 제목";
         String content = "메모 내용";
-        Widget widget = new Widget(null, WidgetType.MEMO);
+        MemoWidgetAddRequest memoWidgetAddRequest = new MemoWidgetAddRequest(lesson.getId(), title, content);
+
+        // when & then
+        assertThatThrownBy(() -> memoWidgetService.addMemoWidget(user, memoWidgetAddRequest))
+                .isInstanceOf(MemoApplicationException.class);
+    }
+
+    @Test
+    void 선생님인_경우_메모를_수정할_수_있다() {
+        // given
+        makeUserTeacherOfLesson();
+
+        String title = "메모 제목";
+        String content = "메모 내용";
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
         entityManager.persist(widget);
         MemoTitle memoTitle = new MemoTitle(title);
         MemoContent memoContent = new MemoContent(content);
@@ -106,7 +197,7 @@ class MemoWidgetServiceTest {
                 updatedContent);
 
         // when
-        MemoWidgetResponse memoWidgetResponse = memoWidgetService.updateMemoWidget(memoWidgetUpdateRequest);
+        MemoWidgetResponse memoWidgetResponse = memoWidgetService.updateMemoWidget(user, memoWidgetUpdateRequest);
 
         // then
         MemoWidget updatedMemoWidget = springDataJpaMemoWidgetRepository.findById(memoWidgetResponse.id()).orElse(null);
@@ -118,9 +209,32 @@ class MemoWidgetServiceTest {
     }
 
     @Test
-    void 메모를_삭제할_수_있다() {
+    void 선생님이_아닌_경우_메모_수정시_예외가_발생한다() {
         // given
-        Widget widget = new Widget(null, WidgetType.MEMO);
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
+        entityManager.persist(widget);
+        MemoTitle memoTitle = new MemoTitle("title");
+        MemoContent memoContent = new MemoContent("content");
+        MemoWidget memoWidget = new MemoWidget(widget.getId(), memoTitle, memoContent);
+        entityManager.persist(memoWidget);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        MemoWidgetUpdateRequest memoWidgetUpdateRequest = new MemoWidgetUpdateRequest(memoWidget.getId(), "u title",
+                "u content");
+
+        // when & then
+        assertThatThrownBy(() -> memoWidgetService.updateMemoWidget(user, memoWidgetUpdateRequest))
+                .isInstanceOf(MemoApplicationException.class);
+    }
+
+    @Test
+    void 선생님인_경우_메모를_삭제할_수_있다() {
+        // given
+        makeUserTeacherOfLesson();
+
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
         entityManager.persist(widget);
         String title = "메모 제목";
         String content = "메모 내용";
@@ -135,9 +249,29 @@ class MemoWidgetServiceTest {
         MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(memoWidget.getId());
 
         // when
-        memoWidgetService.deleteMemoWidget(memoWidgetFindRequest);
+        memoWidgetService.deleteMemoWidget(user, memoWidgetFindRequest);
 
         // then
         assertThat(springDataJpaMemoWidgetRepository.findById(memoWidget.getId())).isEmpty();
+    }
+
+    @Test
+    void 선생님이_아닌_경우_메모_삭제시_예외가_발생한다() {
+        // given
+        Widget widget = new Widget(lesson.getId(), WidgetType.MEMO);
+        entityManager.persist(widget);
+        MemoTitle memoTitle = new MemoTitle("title");
+        MemoContent memoContent = new MemoContent("content");
+        MemoWidget memoWidget = new MemoWidget(widget.getId(), memoTitle, memoContent);
+        entityManager.persist(memoWidget);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        MemoWidgetFindRequest memoWidgetFindRequest = MemoWidgetFindRequest.from(memoWidget.getId());
+
+        // when & then
+        assertThatThrownBy(() -> memoWidgetService.deleteMemoWidget(user, memoWidgetFindRequest))
+                .isInstanceOf(MemoApplicationException.class);
     }
 }
