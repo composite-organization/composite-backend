7a9aafe415d6d71fd7dffdaaf45b6257ba76bf7a
supernovaMK <minkikim2002@gmail.com>
2026-05-23 20:51:43 +0900

    [chore] swagger 명세 수정

--- src/main/java/kr/composite/api/quiz/ui/dto/request/UpdateQuizWidgetStatusRequest.java
+++ src/main/java/kr/composite/api/quiz/ui/dto/request/UpdateQuizWidgetStatusRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 @Schema(description = "퀴즈 위젯 상태 업데이트 요청")
 public record UpdateQuizWidgetStatusRequest(
         @NotNull
-        @Schema(description = "퀴즈 위젯 상태", example = "시작 전,진행 중,완료")
+        @Schema(description = "퀴즈 위젯 상태", example = "시작 전,진행 중,종료")
         String status
 ) {
 
